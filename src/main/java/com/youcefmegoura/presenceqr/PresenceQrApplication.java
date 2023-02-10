8cbb5a30824afca2443950972da51e35356f544b
Youcef Megoura <meg.youcef@yahoo.fr>
2023-02-10 18:55:11 +0100

    Project Init

---
+++ src/main/java/com/youcefmegoura/presenceqr/PresenceQrApplication.java
@@ -0,0 +1,13 @@
+package com.youcefmegoura.presenceqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceQrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenceQrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcefmegoura/presenceqr/PresenceQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcefmegoura.presenceqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceQrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
