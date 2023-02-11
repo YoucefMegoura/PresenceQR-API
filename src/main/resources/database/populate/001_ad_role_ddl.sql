7faa5cdccd55419ec1f40a312586760dc1913199
Youcef Megoura <meg.youcef@yahoo.fr>
2023-02-11 13:29:48 +0100

    Fix: Change adRoleId to long

--- src/main/resources/database/populate/001_ad_role_ddl.sql
+++ src/main/resources/database/populate/001_ad_role_ddl.sql
@@ -1,6 +1,6 @@
 create table ad_role
 (
-    ad_role_id  bigserial
+    ad_role_id  bigint
         primary key,
     isactif     boolean     DEFAULT true NOT NULL,
     created     timestamp   DEFAULT NOW() NOT NULL,
