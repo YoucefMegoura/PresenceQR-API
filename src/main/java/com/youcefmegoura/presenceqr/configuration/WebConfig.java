577490d0699a5b89f67ead5863b4f94fe897d69f
Youcef Megoura <meg.youcef@yahoo.fr>
2023-02-11 23:35:24 +0100

    Fix: Add CrossOrigin WebConfig Class

---
+++ src/main/java/com/youcefmegoura/presenceqr/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package com.youcefmegoura.presenceqr.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") //TODO:: Add
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
