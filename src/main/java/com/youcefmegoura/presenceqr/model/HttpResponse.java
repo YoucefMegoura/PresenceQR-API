e1ac9ec10fd088b134959a3fb1fa702c918ebe1f
Youcef Megoura <meg.youcef@yahoo.fr>
2023-02-14 19:54:07 +0100

    Add HttpResponse model

---
+++ src/main/java/com/youcefmegoura/presenceqr/model/HttpResponse.java
@@ -0,0 +1,29 @@
+package com.youcefmegoura.presenceqr.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+/**
+ * @author youcefmegoura
+ * @created 14/02/2023
+ */
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
+public class HttpResponse {
+    protected LocalDateTime timestamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+}
